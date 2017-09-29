151edc1b30eb80e7ee302ac3d6814f47fa5e5502
Kaji <3masayc@gmail.com>
2017-09-29 10:25:16 +0000

    kadai-tasklist

---
+++ app/controllers/joblists_controller.rb
@@ -0,0 +1,53 @@
+class JoblistsController < ApplicationController
+  def index
+     @joblists = Joblist.all
+  end
+
+  def show
+    @joblist = Joblist.find(params[:id])
+  end
+
+  def new
+    @joblist = Joblist.new
+  end
+
+  def create
+     @joblist = Joblist.new(joblist_params)
+
+    if @joblist.save
+      flash[:success] = 'Tasklist が正常に投稿されました'
+      redirect_to @joblist
+    else
+      flash.now[:danger] = 'Tasklist が投稿されませんでした'
+      render :new
+    end
+  end
+
+  def edit
+    @joblist = Joblist.find(params[:id])
+  end
+
+  def update
+     @joblist = Tasklist.find(params[:id])
+
+    if @joblist.update(joblisd_params)
+      flash[:success] = 'Tasklist は正常に更新されました'
+      redirect_to @joblist
+    else
+      flash.now[:danger] = 'Tasklist は更新されませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+  
+  
+end
+
+private
+
+  # Strong Parameter
+  def joblist_params
+    params.require(:joblist).permit(:content)
+  end
